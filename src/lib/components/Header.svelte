c2a0487434287b3dc4f7acccfc6bb4cfc9942499
Damyan03 <dimi0018@hz.nl>
2025-09-30 11:30:39 +0200

    simple header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+  import skcLogo from '$lib/assets/SKC-logo.png';
+  import profile from '$lib/assets/profile.svg';
+</script>
+
+<!-- change color of bg to predefined colors -->
+<header class="bg-red-500 h-20 w-full flex justify-between">
+    <img src={skcLogo} alt="Logo" class="w-36 h-36"/>
+    <img src={profile} alt="Profile" class="w-16 h-16 mt-2 mr-10"/>
+</header>
