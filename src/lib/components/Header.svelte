4e37471cc730246fea215539a52aaf3dde030764
Ivan Karev <kare0032@hz.nl>
2025-10-02 12:49:34 +0200

    Small adjustments to the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,6 +5,6 @@
 
 <!-- change color of bg to predefined colors -->
 <header class="bg-light-navbar-primary h-20 w-full flex justify-between">
-    <img src={skcLogo} alt="Logo" class="w-36 h-36"/>
-    <img src={profile} alt="Profile" class="w-16 h-16 mt-2 mr-10"/>
+    <img src={skcLogo} alt="Logo" class="w-30 h-30"/>
+    <img src={profile} alt="Profile" class="w-12 h-12 mt-4 mr-10"/>
 </header>
