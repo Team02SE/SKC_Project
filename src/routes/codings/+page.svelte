a86bfc498081b16022941a631017b6d7e085571c
Damyan03 <dimi0018@hz.nl>
2025-10-02 12:27:53 +0200

    editing tab base

--- src/routes/codings/+page.svelte
+++ src/routes/codings/+page.svelte
@@ -5,12 +5,16 @@
 </script>
 
 <Header />
-<div class="flex h-18 w-full items-center justify-between p-4 sticky top-20">
-	<ButtonSvg type="home" size={12} />
+<div class="flex h-18 w-full items-center p-4 sticky top-20">
+    <ButtonSvg type="home" size={12} />
+    <div class="w-px h-10 bg-light-text-primary mx-4"></div>
+    <div class="bg-light-navbar-primary h-full w-64 rounded-t-2xl flex justify-center items-center">
+		<p class="text-light-primary text-medium font-medium px-1">Editing - DOCUMENT_NAME</p>
+	</div>
 </div>
 
 <div class="flex w-full gap-5 px-4 py-2">
-	<div class="sticky top-40 h-[calc(100vh-160px)] w-56 self-start bg-green-500"></div>
+	<div class="sticky top-40 h-[calc(100vh-160px)] w-56 bg-green-500"></div>
 
 	<!-- middle content -->
 	<div class="h-full w-5/12 rounded-2xl bg-light-primary p-5 inset-shadow-sm/25">
