06e4be0d665f7ed04c2db3d5d193388f0008d7dc
michalpij <jaku0001@hz.nl>
2025-10-03 11:11:40 +0200

    Implemented the navbar and created basic routing to other pages.

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+    import ButtonSvg from '$lib/components/ButtonSvg.svelte';
+    import SearchBar from '$lib/components/SearchBar.svelte';
+    import FilterBar from '$lib/components/FilterBar.svelte';
+</script>
+
+<Header/>
+<!-- <UploadComplete/> -->
+<div class="h-18 w-full p-4 flex justify-between items-center">
+    <ButtonSvg type="home" size={12}/>
+    <div class="flex flex-1 justify-end h-full gap-2">
+        <SearchBar/>
+        <FilterBar/>
+    </div>
+</div>
