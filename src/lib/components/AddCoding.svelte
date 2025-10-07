415db01e693c7ca247f052591c0e902708761bf6
Damyan03 <dimi0018@hz.nl>
2025-10-07 14:47:21 +0200

    add coding component

---
+++ src/lib/components/AddCoding.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import ButtonSvg from './ButtonSvg.svelte';
+	import ButtonText from './ButtonText.svelte';
+
+	export let customClass: string = '';
+
+	export let buttonOnClick = () => {};
+</script>
+
+<div class={`flex flex-col gap-2 ${customClass}`}>
+	<div class="flex h-10 w-auto items-center rounded-2xl shadow-md/25 {customClass}">
+		<p class="flex-1 pl-2 font-medium text-light-text-primary">Select...</p>
+		<ButtonSvg type="dropdown" size={6} customClass="mr-2 ml-auto" onClick={buttonOnClick} />
+	</div>
+	<div class="flex justify-end">
+		<ButtonText
+			text="Add"
+			customClass="text-sm bg-light-button-primary rounded-md w-16"
+			onClick={buttonOnClick}
+		/>
+	</div>
+</div>
+
+<div>
+	<h3 class="mb-2 text-lg font-semibold text-light-text-primary">Name</h3>
+	<input
+		type="text"
+		placeholder="Search..."
+		class="h-10 w-full rounded-2xl border-0 bg-transparent px-5 text-lg text-light-text-primary placeholder-light-text-primary/50 shadow-md/25 outline-none"
+	/>
+	<h3 class="mt-4 mb-2 text-lg font-semibold text-light-text-primary">Description</h3>
+	<textarea
+		placeholder="Search..."
+		class="min-h-[120px] w-full resize-none rounded-2xl border-0 bg-transparent px-5 py-3 text-lg text-light-text-primary placeholder-light-text-primary/50 shadow-md/25 outline-none"
+	></textarea>
+    <div class="flex justify-end">
+		<ButtonText
+			text="Add"
+			customClass="text-sm bg-light-button-primary rounded-md w-16"
+			onClick={buttonOnClick}
+		/>
+	</div>
+</div>
--- src/lib/components/CodingsActivities.svelte
+++ src/lib/components/CodingsActivities.svelte
@@ -1,10 +1,11 @@
 <script>
-    import Activity from "./Activity.svelte";
+    import Activity from "./TreeCodings.svelte";
+    import AddCoding from "./AddCoding.svelte";
 </script>
 
 <h1 class="text-4xl font-bold text-light-text-primary">Activities</h1>
 <div class="w-2/3 h-0.5 bg-light-text-primary/15 "></div>
-<div class="flex h-full w-full gap-5">
+<div class="flex h-full w-full gap-30">
     <div class="h-full w-1/2">
         <h2 class="text-2xl font-semibold text-light-text-primary mb-2">N1</h2>
         <Activity />
@@ -12,5 +13,6 @@
     </div>
     <div class="h-full w-1/2">
         <h2 class="text-2xl font-semibold text-light-text-primary mb-2">Add N1 Effect</h2>
+        <AddCoding />
     </div>
 </div>
