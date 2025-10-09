6c5a382474ebeb0f6c48e7447cd8b2d788d77c77
Damyan03 <dimi0018@hz.nl>
2025-10-09 14:17:46 +0200

    final components for the codings

--- src/lib/components/CodingsSV.svelte
+++ src/lib/components/CodingsSV.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import TreeCodings from "./TreeCodings.svelte";
     import AddCoding from "./AddCoding.svelte";
+	import Textbox from "./Textbox.svelte";
 
     // THIS IS TEST DATA UNTIL REAL DATA IS IMPLEMENTED
     const svData = {
@@ -26,7 +27,7 @@
     };
 </script>
 
-<h1 class="text-4xl font-bold text-light-text-primary">System Vulnerabilities</h1>
+<h1 class="text-4xl font-bold text-light-text-primary">System Vulnerabilities - Clustering</h1>
 <div class="flex h-full w-full gap-30">
     <div class="h-full w-1/2">
         <TreeCodings data={svData} />
@@ -36,3 +37,6 @@
         <AddCoding />
     </div>
 </div>
+
+<h1 class="text-4xl font-bold text-light-text-primary">System Vulnerabilities</h1>
+<Textbox />
