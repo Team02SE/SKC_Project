2b0aa43207848de246c51e8e083584da87443e8d
Damyan03 <dimi0018@hz.nl>
2025-10-22 12:57:32 +0200

    scroll func

--- src/lib/components/CodingsEffects.svelte
+++ src/lib/components/CodingsEffects.svelte
@@ -35,16 +35,18 @@
     ];
 </script>
 
-<h1 class="text-4xl font-bold text-light-text-primary">Effects</h1>
-<div class="flex h-full w-full gap-30">
-	<div class="h-full w-1/2">
-		<h2 class="mb-2 text-2xl font-semibold text-light-text-primary">N1</h2>
-        {#each effectsData as effect}
+<div class="mb-50">
+    <h1 class="text-4xl font-bold text-light-text-primary">Effects</h1>
+    <div class="flex h-full w-full gap-30">
+        <div class="h-full w-1/2">
+            <h2 class="mb-2 text-2xl font-semibold text-light-text-primary">N1</h2>
+            {#each effectsData as effect}
             <TreeCodings data={effect} />
-        {/each}
-	</div>
-	<div class="h-full w-1/2">
-		<h2 class="mb-2 text-2xl font-semibold text-light-text-primary">Add N1 Effect</h2>
-		<AddCoding />
-	</div>
+            {/each}
+        </div>
+        <div class="h-full w-1/2">
+            <h2 class="mb-2 text-2xl font-semibold text-light-text-primary">Add N1 Effect</h2>
+            <AddCoding />
+        </div>
+    </div>
 </div>
